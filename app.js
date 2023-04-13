12e9fb4f776a581d29350ed92c2918cdf5bebf2b
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-04-13 04:28:46 -0300

    Creando variables en js para el menu latera, funciones, evento onclick y el menu FUNCIONAAAAAAAA!!!!!

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //Menu lateral
 var menu_visible = false;
 let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//oculto el menu una vez que selecciono una opción
+
