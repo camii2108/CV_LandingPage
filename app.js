e24848fbbd0c421b6816a90f79cff663002b2675
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-04-13 10:42:04 -0300

    funcion menu lateral, me lleva las diferentes secciones y me oculta cuando ya seleccione una opcion

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ function mostrarOcultarMenu(){
     }
 }
 //oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
 
